c7e0da94d12ea21bdeab1577036eb04e87cf5383
Robert Ing <ring@mparticle.com>
2021-11-19 10:03:36 -0500

    docs: Add comments to clarify code changes

--- src/user-attribute-handler.js
+++ src/user-attribute-handler.js
@@ -1,26 +1,27 @@
 function UserAttributeHandler(common) {
     this.common = common || {};
 }
-UserAttributeHandler.prototype.onRemoveUserAttribute = function(
-    key
-    // mParticleUser
-) {
+
+// `mParticleUser` was removed from the function signatures onRemoveUserAttribute, onSetUserAttribute, and onConsentStateUpload because they were not being used
+// In the future if mParticleUser is ever required for an implementation of any of the below APIs, see https://github.com/mparticle-integrations/mparticle-javascript-integration-example/blob/master/src/user-attribute-handler.js
+// for previous function signatures
+
+UserAttributeHandler.prototype.onRemoveUserAttribute = function(key) {
     var userAttributes = {};
     userAttributes[key] = null;
     gtag('set', 'user_properties', userAttributes);
 };
-UserAttributeHandler.prototype.onSetUserAttribute = function(
-    key,
-    value
-    // mParticleUser
-) {
+
+UserAttributeHandler.prototype.onSetUserAttribute = function(key, value) {
     var userAttributes = {};
     userAttributes[key] = value;
     gtag('set', 'user_properties', userAttributes);
 };
-UserAttributeHandler.prototype.onConsentStateUpdated = function() // oldState,
+
+// TODO: Commenting this out for now because Integrations PM still determining if this is in scope or not
+// UserAttributeHandler.prototype.onConsentStateUpdated = function() // oldState,
 // newState,
 // mParticleUser
-{};
+// {};
 
 module.exports = UserAttributeHandler;
