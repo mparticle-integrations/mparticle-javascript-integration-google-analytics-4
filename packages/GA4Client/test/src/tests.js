9aa6c574bb67b1da2a812759674eb8e069ea9efd
Robert Ing <rmi22186@gmail.com>
2023-02-17 15:31:36 -0500

    Update packages/GA4Client/test/src/tests.js
    Co-authored-by: Alex S <49695018+alexs-mparticle@users.noreply.github.com>

--- packages/GA4Client/test/src/tests.js
+++ packages/GA4Client/test/src/tests.js
@@ -1541,7 +1541,7 @@ describe('Google Analytics 4 Event', function () {
         it('should handle onUserIdentified with customerid', function (done) {
             kitSettings.externalUserIdentityType = 'CustomerId';
             mParticle.forwarder.init(kitSettings, reportService.cb, true, null);
-            dataLayer = [];
+            window.dataLayer = [];
             mParticle.forwarder.onUserIdentified(mParticleUser);
 
             var result = [
