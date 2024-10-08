5a8346f3ef421fdeb0bd55778251915e89753b0c
Mo Mustafa <mmustafa@mparticle.com>
2024-10-08 12:05:28 -0700

    fix:send default and update consent paylaods on kit init

--- packages/GA4Client/src/initialization.js
+++ packages/GA4Client/src/initialization.js
@@ -115,12 +115,12 @@ var initialization = {
                 common.consentMappings
             );
 
+        // If a default consent payload exists (as selected in the mParticle UI), set it as the default
         if (!common.isEmpty(defaultConsentPayload)) {
-            // If a default consent payload exists (as selected in the mParticle UI), set it as the default
             common.sendDefaultConsentPayloadToGoogle(defaultConsentPayload);
+        // If a default consent payload does not exist, but the user currently has updated their consent,
+        // send that as the default because a default must be sent
         } else if (!common.isEmpty(updatedDefaultConsentPayload)) {
-            // If a default consent payload does not exist, but the user currently has updated their consent,
-            // send that as the default because a default must be sent
             common.sendDefaultConsentPayloadToGoogle(
                 updatedDefaultConsentPayload
             );
